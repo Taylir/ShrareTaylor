3b1b6fc532cde3a137fc180b16b22892aead87e1
taylir <tdl9716@gmail.com>
2023-03-11 11:14:32 -0500

    Added redirect for unlogged user

--- shareme_frontend/src/App.jsx
+++ shareme_frontend/src/App.jsx
@@ -3,8 +3,17 @@ import { Routes, Route, useNavigate } from "react-router-dom";
 
 import Login from "./components/Login";
 import Home from "./container/Home";
+import { fetchUser } from "./utils/fetchUser";
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const user = fetchUser();
+
+    if (!user) navigate("/login");
+  }, []);
+
   return (
     <Routes>
       <Route path="login" element={<Login />} />
