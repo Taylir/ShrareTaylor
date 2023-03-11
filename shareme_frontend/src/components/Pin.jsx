bee44b665b71552433832d3cba070118a306735c
taylir <tdl9716@gmail.com>
2023-03-11 14:45:23 -0500

    Made more tools for mobile

--- shareme_frontend/src/components/Pin.jsx
+++ shareme_frontend/src/components/Pin.jsx
@@ -67,73 +67,71 @@ function Pin({ pin: { postedBy, image, _id, destination, save } }) {
           src={urlFor(image).width(250).url()}
           alt="user-post"
         />
-        {postHovered && (
-          <div
-            style={{ height: "100%" }}
-            className="absolute top-0 w-full h-full flex flex-col justify-between p-1 pr-2 pt-2 pb-2 z-50"
-          >
-            <div className="flex items-center justify-between">
-              <div className="flex gap-2">
-                <a
-                  href={`${image?.asset?.url}?dl=`}
-                  download
-                  onClick={(e) => e.stopPropagation()}
-                  className="bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none"
-                >
-                  <MdDownloadForOffline />
-                </a>
-              </div>
-              {alreadySaved ? (
-                <button
-                  type="button"
-                  className="bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outline-none"
-                >
-                  {" "}
-                  {save?.length} Saved
-                </button>
-              ) : (
-                <button
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    savePin(_id);
-                  }}
-                  type="button"
-                  className="bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outline-none"
-                >
-                  {savingPost ? "Saving" : "Save"}
-                </button>
-              )}
+        <div
+          style={{ height: "100%" }}
+          className="absolute top-0 w-full h-full flex flex-col justify-between p-1 pr-2 pt-2 pb-2 z-50"
+        >
+          <div className="flex items-center justify-between">
+            <div className="flex gap-2">
+              <a
+                href={`${image?.asset?.url}?dl=`}
+                download
+                onClick={(e) => e.stopPropagation()}
+                className="bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none"
+              >
+                <MdDownloadForOffline />
+              </a>
             </div>
-            <div className="flex justify-between items-center gap-2 w-full">
-              {destination && (
-                <a
-                  href={destination}
-                  target="_blank"
-                  rel="noreferrer"
-                  className="bg-white flex items-center gap-2 text-black font-bold p-2 pl-4 pr-4 rounded-full opacity-70 hover:opacity-100 hover:shadow-md"
-                  onClick={(e) => e.stopPropagation}
-                >
-                  <BsFillArrowUpRightCircleFill />
-                  {destination.length > 15
-                    ? `${destination.slice(0, 15)}...`
-                    : { destination }}
-                </a>
-              )}
-              {postedBy?._id === user.sub && (
-                <button
-                  type="button"
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    deletePin(_id);
-                  }}
-                  className="bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none"
-                >
-                  <AiTwotoneDelete />
-                </button>
-              )}
-            </div>
+            {alreadySaved ? (
+              <button
+                type="button"
+                className="bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outline-none"
+              >
+                {" "}
+                {save?.length} Saved
+              </button>
+            ) : (
+              <button
+                onClick={(e) => {
+                  e.stopPropagation();
+                  savePin(_id);
+                }}
+                type="button"
+                className="bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outline-none"
+              >
+                {savingPost ? "Saving" : "Save"}
+              </button>
+            )}
           </div>
-        )}
+          <div className="flex justify-between items-center gap-2 w-full">
+            {destination && (
+              <a
+                href={destination}
+                target="_blank"
+                rel="noreferrer"
+                className="bg-white flex items-center gap-2 text-black font-bold p-2 pl-4 pr-4 rounded-full opacity-70 hover:opacity-100 hover:shadow-md"
+                onClick={(e) => e.stopPropagation}
+              >
+                <BsFillArrowUpRightCircleFill />
+                {destination.length > 15
+                  ? `${destination.slice(0, 15)}...`
+                  : { destination }}
+              </a>
+            )}
+            {postedBy?._id === user.sub && (
+              <button
+                type="button"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  deletePin(_id);
+                }}
+                className="bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none"
+              >
+                <AiTwotoneDelete />
+              </button>
+            )}
+          </div>
+        </div>
       </div>
       <Link
         to={`user-profile/${postedBy?._id}`}
